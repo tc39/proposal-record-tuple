d48548f40eaaa996c4a7a215940fac94766ef921
Richard Button <rbutton2@bloomberg.net>
2019-11-25 18:35:40 +0000

    use locally published babel

--- babel-plugin-experiment/babel.config.js
+++ babel-plugin-experiment/babel.config.js
@@ -1,9 +1,7 @@
 module.exports = function (api) {
     api.cache(true);
 
-    const presets = [
-        "@babel/preset-env",
-    ];
+    const presets = ["@babel/preset-env"];
     const plugins = [];
 
     return {
