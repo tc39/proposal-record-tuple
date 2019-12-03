37c2208dd0ee682e8ed01719ec6611efe0386ec8
Richard Button <rbutton2@bloomberg.net>
2019-12-03 20:16:57 +0000

    added repl

--- babel-plugin-experiment/Gulpfile.js
+++ babel-plugin-experiment/Gulpfile.js
@@ -3,33 +3,86 @@ const babel = require("gulp-babel");
 const watch = require("gulp-watch");
 const through = require("through2");
 const path = require("path");
+const webpack = require("webpack-stream");
+const merge = require("merge-stream");
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 const base = __dirname;
-function swapSrcWithLib(file) {
-    const parts = file.relative.split(path.sep);
-    parts[1] = "lib";
-    const newRelative = parts.join(path.sep);
-    return path.resolve(base, "packages", newRelative);
-}
 
-function rename(fn) {
-    return through.obj(function (file, enc, callback) {
-        file.path = fn(file);
-        console.log(file.path);
-        callback(null, file);
-    })
-}
+const pkgToSource = pkg => `packages/${pkg}/src/**/*.js`;
+const sources = [
+    "babel-plugin-proposal-record-and-tuple",
+    "record-and-tuple-polyfill",
+];
+const webpackSources = [
+    "record-and-tuple-repl",
+];
+const babelConfig = {
+    presets: ["@babel/preset-env", "@babel/preset-react"],
+};
 
-const sources = "./packages/*/src/**/*.js";
+const webpackConfig = {
+    mode: process.env.NODE_ENV || "development",
+    context: path.resolve("./packages/record-and-tuple-repl"),
+    entry: {
+        "index": "./src/index.jsx",
+    },
+    output: {
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: babelConfig,
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [new MonacoWebpackPlugin({
+        languages: ["javascript"],
+    }), new CopyPlugin([
+        { from: "src/index.html", to: "index.html" }
+    ])],
+};
 
-gulp.task("build", () => {
-
-    return gulp.src(sources)
-        .pipe(babel())
-        .pipe(rename(file => swapSrcWithLib(file)))
-        .pipe(gulp.dest(path.resolve(base, "packages")));
+gulp.task("build-infra", () => {
+    const builds = sources.map(source => {
+        return gulp.src(pkgToSource(source))
+            .pipe(babel(babelConfig))
+            .pipe(gulp.dest(path.resolve(base, "packages", source, "lib")));
+    });
+    return merge(...builds);
 });
 
-gulp.task("watch", gulp.series("build", function () {
-    watch(sources, { debounceDelay: 200 }, gulp.task("build"));
+gulp.task("build-repl", () => {
+    const builds = webpackSources.map(source => {
+        return gulp.src(pkgToSource(source))
+            .pipe(webpack(webpackConfig))
+            .pipe(gulp.dest(path.resolve(base, "packages", source, "lib")));
+    });
+    return merge(...builds);
+});
+
+gulp.task("watch-repl", () => {
+    const builds = webpackSources.map(source => {
+        return gulp.src(pkgToSource(source))
+            .pipe(webpack(Object.assign({}, webpackConfig, { watch: true })))
+            .pipe(gulp.dest(path.resolve(base, "packages", source, "lib")));
+    });
+    return merge(...builds);
+});
+
+gulp.task("build", gulp.series("build-infra", "build-repl"));
+
+gulp.task("watch-infra", gulp.series("build", function () {
+    watch(sources, { debounceDelay: 200 }, gulp.task("build-infra"));
 }));
---
+++ babel-plugin-experiment/packages/record-and-tuple-repl/src/index.jsx
@@ -0,0 +1,216 @@
+import * as Babel from "@babel/core";
+import RecordAndTuple from "babel-plugin-proposal-record-and-tuple";
+import PresetEnv from "@babel/preset-env";
+import * as Polyfill from "record-and-tuple-polyfill";
+
+import React from "react";
+import { render } from "react-dom";
+import { Hook, Console, Decode } from "console-feed";
+import MonacoEditor from "react-monaco-editor";
+
+import Normalize from "./normalize.css";
+import Skeleton from "./skeleton.css";
+
+// dirty hack to not require bundling of the output of babel
+window.require = function(path) {
+    if (path !== "record-and-tuple-polyfill") {
+        throw new Error("unexpected");
+    }
+    return Polyfill;
+}
+
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function executedFunction() {
+        const context = this;
+        const args = arguments;
+        const callNow = immediate && !timeout;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+const CONSOLE_STYLES = {
+    LOG_ICON_WIDTH: "0px",
+    LOG_ICON_HEIGHT: "0px",
+    BASE_FONT_SIZE: "30px",
+    BASE_LINE_HEIGHT: "41px",
+    ARROW_FONT_SIZE: "30px",
+    TREENODE_FONT_SIZE: "30px",
+    TREENODE_LINE_HEIGHT: "41px",
+};
+
+const DEFAULT_PREFIX = String.raw`import { equal, Record, Tuple } from "record-and-tuple-polyfill";
+const log = console.log;
+`;
+
+const DEFAULT_HASH = DEFAULT_PREFIX + String.raw`
+const record = #{ prop: 1 };
+const tuple = #[1, 2, 3];
+
+log(equal(#{ a: 1 }, #{ a: 1 }));
+`;
+
+const DEFAULT_BAR = DEFAULT_PREFIX + String.raw`
+
+const record = {| prop: 1 |};
+const tuple = [|1, 2, 3|];
+
+log(equal(record, tuple));
+`;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            output: "",
+            syntax: "hash",
+            logs: [],
+        };
+
+        this.onChange = debounce(this.onChange.bind(this), 500);
+        this.onSyntaxChange = this.onSyntaxChange.bind(this);
+        this.update = this.update.bind(this);
+
+        this.value = DEFAULT_HASH;
+        this.outputEditor = React.createRef();
+
+        const methods = ["log", "warn", "error", "info", "debug", "command", "result"];
+        this.fakeConsole = methods.reduce((obj, m) => {
+            obj[m] = () => undefined;
+            return obj;
+         }, {})
+    }
+
+    componentDidMount() {
+        Hook(this.fakeConsole, log => {
+            this.setState(({ logs }) => ({ logs: [...logs, Decode(log)] }));
+        });
+    }
+
+    render() {
+        const editorOptions = {
+            fontSize: 30,
+            language: "javascript",
+            theme: "vs-dark",
+            automaticLayout: true,
+            codeLens: false,
+            minimap: {
+                enabled: false,
+            }
+        };
+
+        const outputOptions = Object.assign({}, editorOptions, {
+            readOnly: true,
+        });
+
+        return (
+            <div className="container">
+                <div className="topBar">
+                    <div className="left">
+                        <span>Syntax Type:</span>
+                        <select onChange={this.onSyntaxChange} value={this.state.syntax}>
+                            <option value="hash">hash</option>
+                            <option value="bar">bar</option>
+                        </select>
+                    </div>
+                    <div className="right">
+                        <span>Record and Tuple Playground</span>
+                        <span><a href="https://github.com/tc39/proposal-record-tuple">Proposal</a></span>
+                        <span><a href="https://github.com/tc39/proposal-record-tuple/blob/rb/babel-experiment/babel-plugin-experiment/packages/record-and-tuple-polyfill/src/index.js">Polyfill</a></span>
+                    </div>
+                </div>
+                <div className="inputWrapper">
+                    <MonacoEditor
+                        options={editorOptions}
+                        value={this.value}
+                        editorDidMount={this.update}
+                        onChange={this.onChange} />
+                </div>
+                <div className="outputWrapper">
+                    <div style={{ float: "right", width: "100%", height: "60%" }}>
+                        <MonacoEditor
+                            ref={this.outputEditor}
+                            value={this.state.output}
+                            options={outputOptions} />
+                    </div>
+                    <div style={{ borderTop: "2px solid #303030", float: "right", width: "100%", height: "40%" }}>
+                        <Console
+                            styles={CONSOLE_STYLES}
+                            variant="dark"
+                            filter={["log", "info", "error"]} logs={this.state.logs} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    onChange(newValue) {
+        this.value = newValue;
+        this.update();
+    }
+
+    onSyntaxChange(event) {
+        const syntax = event.target.value;
+
+        this.value = syntax === "hash" ? DEFAULT_HASH : DEFAULT_BAR;
+        this.setState({
+            syntax,
+            output: "",
+        }, () => {
+            this.update();
+        });
+    }
+
+    update() {
+        this.transform(this.value, (err, result) => {
+            const output = err ? err.toString() : result;
+            this.setState({ output, logs: [], }, () => {
+                this.outputEditor.current.editor.setSelection({
+                    startLineNumber: 1,
+                    startColumn: 1,
+                    endLineNumber: 1,
+                    endColumn: 1,
+                });
+                if (!err) {
+                    this.run();
+                }
+            });
+        });
+    }
+
+    transform(code, callback) {
+        const pluginOptions = this.state.syntax === "hash" ? { hash: true } : { bar: true };
+
+        const options = {
+            presets: [PresetEnv],
+            plugins: [
+                [RecordAndTuple, pluginOptions]
+            ],
+        };
+        Babel.transform(code, options, function (err, result) {
+            callback(err, result ? result.code : undefined);
+        });
+    }
+
+    run() {
+        try {
+            const func = new Function("console", this.state.output);
+            func(this.fakeConsole);
+        } catch(e) {
+            this.fakeConsole.error(e.message);
+            this.fakeConsole.error(e);
+        }
+    }
+}
+
+render(
+    <App />,
+    document.getElementById("root"),
+);
