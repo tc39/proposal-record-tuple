401caf7e8619312909d64ecac62be283683e3762
Robin Ricard <rricard2@bloomberg.net>
2020-07-09 16:20:09 +0100

    Add syntax highlighter in cookbook

--- cookbook/config.mjs
+++ cookbook/config.mjs
@@ -1,33 +1,33 @@
 import { join, dirname } from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
-    languages: ["en", "fr"],
-    defaultLanguage: "en",
-    watchDir: __dirname,
-    template: join(__dirname, "template.html"),
-    copyToOutDir: join(__dirname, "assets"),
-    outDir: join(__dirname, "..", "out", "cookbook"),
-    replacementsPerLanguage: {
-        en: {
-            RT_COOKBOOK_TITLE: "Record & Tuple Cookbook",
-            RUN_IN_PLAYGROUND: "Run in playground",
-            TOP_OF_PAGE: "^ Back to top"
-        },
-        fr: {
-            RT_COOKBOOK_TITLE: "Livre de recettes Record & Tuple",
-            RUN_IN_PLAYGROUND: "Essayer dans le bac à sable",
-            TOP_OF_PAGE: "^ Retour en haut de page"
-        }
+  languages: ["en", "fr"],
+  defaultLanguage: "en",
+  watchDir: __dirname,
+  template: join(__dirname, "template.html"),
+  copyToOutDir: join(__dirname, "assets"),
+  outDir: join(__dirname, "..", "out", "cookbook"),
+  replacementsPerLanguage: {
+    en: {
+      RT_COOKBOOK_TITLE: "Record & Tuple Cookbook",
+      RUN_IN_PLAYGROUND: "Run in playground",
+      TOP_OF_PAGE: "^ Back to top",
     },
-    markdownFilesReplacementsPerLanguage: {
-        en: {
-            COOKBOOK_CONTENTS: join(__dirname, "en.md"),
-        },
-        fr: {
-            COOKBOOK_CONTENTS: join(__dirname, "fr.md"),
-        },
-    }
-}
+    fr: {
+      RT_COOKBOOK_TITLE: "Livre de recettes Record & Tuple",
+      RUN_IN_PLAYGROUND: "Essayer dans le bac à sable",
+      TOP_OF_PAGE: "^ Retour en haut de page",
+    },
+  },
+  markdownFilesReplacementsPerLanguage: {
+    en: {
+      COOKBOOK_CONTENTS: join(__dirname, "en.md"),
+    },
+    fr: {
+      COOKBOOK_CONTENTS: join(__dirname, "fr.md"),
+    },
+  },
+};
